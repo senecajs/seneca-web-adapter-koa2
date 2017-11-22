ef7ac5c25ca2f01d5788c765bd3d38dfa3a7f868
Tyler Waters <tyler.waters@gmail.com>
2017-11-21 21:30:36 -0800

    Adds support for middleware
    Fixes #19

--- seneca-web-adapter-koa2.js
+++ seneca-web-adapter-koa2.js
@@ -6,13 +6,25 @@ const Parse = require('co-body')
 module.exports = function koa (options, context, auth, routes, done) {
   const seneca = this
 
+  // middleware is an object with keys defining middleware
+  const middleware = options.middleware
+
   if (!context) {
     return done(new Error('no context provided'))
   }
 
   _.each(routes, route => {
+    // pull out middleware from the route; map strings to options' middleware.
+    // if we don't get a function, blow up hard - this is a user-code problem.
+    const routeMiddleware = (route.middleware || []).map(_middleware => {
+      const ret = _.isString(_middleware) ? middleware[_middleware] : _middleware
+      if (!_.isFunction(ret)) {
+        throw new Error(`expected valid middleware, got ${_middleware}`)
+      }
+      return ret
+    })
     _.each(route.methods, method => {
-      context[method.toLowerCase()](route.path, async (ctx, next) => {
+      context[method.toLowerCase()](route.path, ...routeMiddleware, async (ctx, next) => {
         let body = {}
 
         if (['POST', 'PUT'].indexOf(ctx.req.method) > -1) {
