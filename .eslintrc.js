dddc0a283798e6bff98d062a2f359e556297791e
EamonnK <amen_cionna@hotmail.com>
2017-12-25 00:05:46 +0000

    Code quality control updated - .eslintrc.js added/prettier installed

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    node: true,
+    es6: true
+  },
+  "parserOptions": {
+    "ecmaVersion": 8
+  },
+  rules: {
+    'no-console': 0
+  }
+}
--- seneca-web-adapter-koa2.js
+++ seneca-web-adapter-koa2.js
@@ -3,7 +3,7 @@
 const _ = require('lodash')
 const Parse = require('co-body')
 
-module.exports = function koa (options, context, auth, routes, done) {
+module.exports = function koa(options, context, auth, routes, done) {
   const seneca = this
 
   // middleware is an object with keys defining middleware
@@ -17,54 +17,59 @@ module.exports = function koa (options, context, auth, routes, done) {
     // pull out middleware from the route; map strings to options' middleware.
     // if we don't get a function, blow up hard - this is a user-code problem.
     const routeMiddleware = (route.middleware || []).map(_middleware => {
-      const ret = _.isString(_middleware) ? middleware[_middleware] : _middleware
+      const ret = _.isString(_middleware)
+        ? middleware[_middleware]
+        : _middleware
       if (!_.isFunction(ret)) {
         throw new Error(`expected valid middleware, got ${_middleware}`)
       }
       return ret
     })
     _.each(route.methods, method => {
-      context[method.toLowerCase()](route.path, ...routeMiddleware, async (ctx, next) => {
-        let body = {}
+      context[method.toLowerCase()](
+        route.path,
+        ...routeMiddleware,
+        async (ctx) => {
+          let body = {}
 
-        if (['POST', 'PUT'].indexOf(ctx.req.method) > -1) {
-          body = options.parseBody === false
-            ? ctx.request.body
-            : await Parse(ctx)
-        }
+          if (['POST', 'PUT'].indexOf(ctx.req.method) > -1) {
+            body =
+              options.parseBody === false ? ctx.request.body : await Parse(ctx)
+          }
 
-        const query = Object.assign({}, ctx.request.query)
-        const params = ctx.params
+          const query = Object.assign({}, ctx.request.query)
+          const params = ctx.params
 
-        const payload = {
-          request$: ctx.request,
-          response$: ctx.response,
-          args: {body, query, params}
-        }
+          const payload = {
+            request$: ctx.request,
+            response$: ctx.response,
+            args: { body, query, params }
+          }
 
-        ctx.response.type = 'json'
+          ctx.response.type = 'json'
 
-        await new Promise((resolve, reject) => {
-          seneca.act(route.pattern, payload, (err, res) => {
-            if (err) {
-              return reject(err)
-            }
+          await new Promise((resolve, reject) => {
+            seneca.act(route.pattern, payload, (err, res) => {
+              if (err) {
+                return reject(err)
+              }
 
-            ctx.status = 200
+              ctx.status = 200
 
-            if (route.redirect) {
-              ctx.redirect(route.redirect)
-            }
+              if (route.redirect) {
+                ctx.redirect(route.redirect)
+              }
 
-            if (route.autoreply) {
-              ctx.body = res
-            }
-            return resolve()
+              if (route.autoreply) {
+                ctx.body = res
+              }
+              return resolve()
+            })
           })
-        })
-      })
+        }
+      )
     })
   })
 
-  done(null, {routes})
+  done(null, { routes })
 }
