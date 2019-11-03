b61c773511a9ecdcc910508da096469adaec8e97
Tyler Waters <tyler.waters@gmail.com>
2019-11-02 23:22:27 -0700

    Bring the library up-to-snuff
    - nyc for coverage
    - mocha for tests
    - upgrade all dependencies
    - apply prettier formatting
    - bit of a rewrite (remove lodash dep)

--- seneca-web-adapter-koa2.js
+++ seneca-web-adapter-koa2.js
@@ -1,75 +1,63 @@
 'use strict'
 
-const _ = require('lodash')
 const Parse = require('co-body')
 
-module.exports = function koa(options, context, auth, routes, done) {
+module.exports = function koa(
+  { middleware: definedMiddleware, parseBody },
+  context,
+  auth,
+  routes,
+  done
+) {
   const seneca = this
-
-  // middleware is an object with keys defining middleware
-  const middleware = options.middleware
 
   if (!context) {
     return done(new Error('no context provided'))
   }
 
-  _.each(routes, route => {
-    // pull out middleware from the route; map strings to options' middleware.
-    // if we don't get a function, blow up hard - this is a user-code problem.
-    const routeMiddleware = (route.middleware || []).map(_middleware => {
-      const ret = _.isString(_middleware)
-        ? middleware[_middleware]
-        : _middleware
-      if (!_.isFunction(ret)) {
-        throw new Error(`expected valid middleware, got ${_middleware}`)
-      }
-      return ret
-    })
-    _.each(route.methods, method => {
-      context[method.toLowerCase()](
-        route.path,
-        ...routeMiddleware,
-        async (ctx) => {
+  routes.forEach(
+    ({ autoreply, redirect, pattern, methods, path, middleware }) => {
+      const routeMiddleware = (middleware || []).map(_m =>
+        typeof _m === 'string' ? definedMiddleware[_m] : _m
+      )
+      methods.forEach(method =>
+        context[method.toLowerCase()](path, ...routeMiddleware, async ctx => {
           let body = {}
 
           if (['POST', 'PUT'].indexOf(ctx.req.method) > -1) {
-            body =
-              options.parseBody === false ? ctx.request.body : await Parse(ctx)
+            body = parseBody === false ? ctx.request.body : await Parse(ctx)
           }
 
-          const query = Object.assign({}, ctx.request.query)
-          const params = ctx.params
-
-          const payload = {
-            request$: ctx.request,
-            response$: ctx.response,
-            args: { body, query, params }
-          }
+          const res = await new Promise((resolve, reject) =>
+            seneca.act(
+              pattern,
+              {
+                request$: ctx.request,
+                response$: ctx.response,
+                args: {
+                  body,
+                  query: { ...ctx.request.query },
+                  params: { ...ctx.params }
+                }
+              },
+              (err, res) => (err ? reject(err) : resolve(res))
+            )
+          )
 
           ctx.response.type = 'json'
+          ctx.status = 200
 
-          await new Promise((resolve, reject) => {
-            seneca.act(route.pattern, payload, (err, res) => {
-              if (err) {
-                return reject(err)
-              }
+          if (redirect) {
+            ctx.redirect(redirect)
+          }
 
-              ctx.status = 200
-
-              if (route.redirect) {
-                ctx.redirect(route.redirect)
-              }
-
-              if (route.autoreply) {
-                ctx.body = res
-              }
-              return resolve()
-            })
-          })
-        }
+          if (autoreply) {
+            ctx.body = res
+          }
+        })
       )
-    })
-  })
+    }
+  )
 
   done(null, { routes })
 }
